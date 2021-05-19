3aa0c937a05492c95b9eb2c4b981cc56fa2db119
M-java86 <mpythondata@outlook.com>
2021-05-19 10:25:04 -0400

    MO: Initial commit

---
+++ src/com/qa/arrays/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.arrays;
+
+class Runner {
+
+	public static void main(String[] args) {
+		method();
+	
+	}
+		public static void method() {
+			System.out.println("Hello World!");
+		}
+}
